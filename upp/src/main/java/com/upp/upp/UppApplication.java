f099747eff6256b207d5d54845c212cb92d5b868
Nemanja <nemanja.gavrilovic1995@gmail.com>
2019-01-12 15:31:30 +0100

    Initial commit

---
+++ upp/src/main/java/com/upp/upp/UppApplication.java
@@ -0,0 +1,14 @@
+package com.upp.upp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppApplication.class, args);
+	}
+
+}
+
